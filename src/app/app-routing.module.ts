eda4af62516196549e12969aa74ac15e35b010b6
AnaswaraDev <anaswaradev1998@gmail.com>
2021-01-12 01:12:48 -1100

    firstcommit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Home1Component } from './home1/home1.component';
+import { Home2Component } from './home2/home2.component';
+import { Home3Component } from './home3/home3.component';
+import { Home4Component } from './home4/home4.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component :Home1Component },
+  {path : 'home2', component :Home2Component },
+  {path : 'home3', component :Home3Component },
+  {path : 'home4', component :Home4Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Home1Component } from './home1/home1.component';
+import { Home2Component } from './home2/home2.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Home3Component } from './home3/home3.component';
+import { Home4Component } from './home4/home4.component';
+import {MaterialModule} from './material/material.module';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Home1Component,
+    Home2Component,
+    Home3Component,
+    Home4Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    NgbModule,
+    MaterialModule,
+    MatInputModule,
+    MatIconModule
+  
+
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
